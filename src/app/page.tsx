f41d0ee5ec70a54892cef5f91186d2c773143ab8
Narutchai <koonfluk2015@gmail.com>
2025-02-28 13:42:27 +0700

    add Zustand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { Idata } from "@/interfaces/data";
 import { useState } from "react";
+import {
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 
 export default function Home() {
   const [input, setInput] = useState<Idata>({
@@ -98,56 +105,47 @@ export default function Home() {
     return Math.exp(w) / (1 + Math.exp(w));
   };
 
-  const lft_ca125 = (
+  const lft_ca125_2 = (
     Menopausal: number,
     Albumin: number,
     Globulin: number,
     CA125: number
   ) => {
     const w =
-      -1.369 +
-      0.536 * Menopausal -
-      0.556 * Albumin +
-      0.565 * Globulin +
-      0.003 * CA125;
+      -2.783 +
+     ( 0.8 * Menopausal) +
+      (0.256 * Globulin) +
+      (0.003 * CA125);
 
     return Math.exp(w) / (1 + Math.exp(w));
   };
 
-  console.log("สูตร 1 ", cbc_us_ca125(1, 247, 1, 1, 1, 43.9).toFixed(4));
-  console.log("สูตร 2 ", us_ca125(1, 1, 1, 1, 43.9).toFixed(4));
-  console.log("สูตร 3 ", cbc_us(1, 247, 1, 1, 1).toFixed(4));
-  console.log("สูตร 4 ", us(1, 1, 1, 1, 1).toFixed(4));
-  console.log("สูตร 5 ", lft_ca125(1, 4.6, 3.7, 43.9).toFixed(4));
+  // console.log("สูตร 1 ", cbc_us_ca125(1, 471, 1, 1, 0, 3988));
+  // console.log("สูตร 2 ", us_ca125(1, 1, 1, 0, 3988));
+  // console.log("สูตร 3 ", cbc_us(1, 471, 1, 1, 0));
+  // console.log("สูตร 4 ", us(1, 1, 1, 1, 0));
+  // console.log("สูตร 5 ", lft_ca125(1, 2.9, 3.1, 3988));
+  console.log("_____________________________________");
+  console.log("สูตร 1 ", cbc_us_ca125(1,493,1,0,0,751));
+  console.log("สูตร 2 ", us_ca125(1,1,0,0,751));
+  console.log("สูตร 3 ", cbc_us(1,493,1,0,0));
+  console.log("สูตร 4 ", us(1,1,0,1,0));
+  console.log("สูตร 5 ", lft_ca125_2(1,3.1,4.4,751));
 
   return (
     <div>
-      <form>
-        <input
-          type="number"
-          placeholder="albuin"
-          name="albuin"
-          onChange={onChange}
-        />
-        <input
-          type="text"
-          placeholder="platelets"
-          name="platelets"
-          onChange={onChange}
-        />
-        <input
-          type="text"
-          placeholder="globulin"
-          name="globulin"
-          onChange={onChange}
-        />
-        <input
-          type="text"
-          placeholder="globulin"
-          name="globulin"
-          onChange={onChange}
-        />
-      </form>
+      <FormControl>
+        <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+        >
+          <FormControlLabel value="female" control={<Radio />} label="Female" />
+          <FormControlLabel value="male" control={<Radio />} label="Male" />
+          <FormControlLabel value="other" control={<Radio />} label="Other" />
+        </RadioGroup>
+      </FormControl>
     </div>
   );
 }
